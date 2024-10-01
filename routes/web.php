45aa0022723458cb0c8ee66303a41c1f42e24008
RomainDoyen <romain.doyen97@gmail.com>
2024-10-01 17:22:00 +0400

    feat: Adding a Dashboard view and controller

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AppController;
 
 Route::get('/', [AuthController::class, 'login'])->name('login');
 Route::post('/', [AuthController::class, 'handleLogin'])->name('handleLogin');
 
 // Route sécurisé
-// Route::get('/dashboard', [])->name('dashboard');
+Route::get('/dashboard', [AppController::class, 'index'])->name('dashboard');

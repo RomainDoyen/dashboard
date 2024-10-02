413d44d43deaaf38cbaf39fd0f1f4482e4ef61bf
RomainDoyen <romain.doyen97@gmail.com>
2024-10-02 18:00:29 +0400

    refactor: Stats display

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Employer;
 use Illuminate\Http\Request;
+use App\Models\Departement;
+use App\Models\User;
 
 class AppController extends Controller
 {
     public function index() {
-        return view('dashboard');
+        $totalDepartements = Departement::all()->count();
+        $totalEmployers = Employer::all()->count();
+        $totalAdmins = User::all()->count();
+        return view('dashboard', compact('totalDepartements', 'totalEmployers', 'totalAdmins'));
     }
 }

6fd8caa5192e75859841f2eb9a45e59950ca28ee
Design And Creation <romain.doyen97@gmail.com>
2024-10-31 17:10:08 +0400

    feat: Customize avatar in topBar

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -135,7 +135,9 @@
 
           <div class="app-utility-item app-user-dropdown dropdown">
             <a class="dropdown-toggle" id="user-dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
-              aria-expanded="false"><img src="assets/images/user.png" alt="user profile"></a>
+              aria-expanded="false">
+              <img src="https://ui-avatars.com/api/?name={{auth()->user()->name}}" alt="user profile" style="border-radius: 50%">
+            </a>
             <ul class="dropdown-menu" aria-labelledby="user-dropdown-toggle">
               <li><a class="dropdown-item" href="account.html">Account</a></li>
               <li><a class="dropdown-item" href="settings.html">Settings</a></li>
